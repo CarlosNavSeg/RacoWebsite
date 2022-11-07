886a7a1203c2439bd85594f5d4571e1e5107ae52
CarlosNavSeg <carlos.raco@hotmail.com>
2022-11-07 12:13:59 +0100

    Proyecto interdisciplinar.

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.getElementById('contacto').onclick = alertarInfo
+}
+
+function alertarInfo() {
+    this.innerHTML = "Telefono de contacto: 638564525 <br/> Email: racocasatere12@gmail.com" 
+}
